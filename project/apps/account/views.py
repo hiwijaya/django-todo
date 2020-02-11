f498f9c386734ec044647a9212a70af66cf45d1c
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-02-11 23:29:54 +0700

    template variable

--- project/apps/account/views.py
+++ project/apps/account/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'account/auth.html')
+    context = {
+        'title': 'Authentication'
+    }
+    return render(request, 'account/auth.html', context)
